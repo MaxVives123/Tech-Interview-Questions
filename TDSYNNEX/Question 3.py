cbb6ada860ba73b957cd7a043a21492b979df012
Max Vives <167073896+MaxVives123@users.noreply.github.com>
2024-05-29 12:35:31 +0200

    Update Question 3.py

--- TDSYNNEX/Question 3.py
+++ TDSYNNEX/Question 3.py
@@ -1,3 +1,24 @@
+# Exercise 3
+# Below you can find a client’s list with information:
+# clientList = [['id billing', 'id client', 'Date of billing', 'Amount'], 
+#               [1, 1, 'Thursday 22:00', 123], 
+#               [2, 2, 'Thursday 10:00', 35], 
+#               [3, 3, 'Wednesday 15:00', 100], 
+#               [4, 3, 'Friday 16:00', 456]]
+
+# · You must create a Main class with a function main.
+# · You must create another class (name it as desired) with the functions detailed below.
+# · You must initialize one object of the previously created class and then, call all the functions
+#   mentioned below.
+# 1. Generate a dataframe with above list (clientList)
+# 2. Remove rows filtering by one date. (date of billing. Field) (parameter date Will be
+#    formatted as the list format date. Be careful with the errors, control them.)
+# 3. Remove column (parameter Will be one column name or none or one missing
+#    column).
+# 4. Create a new dataframe with only the columns id_client and amounts. No duplicates
+#    on id_client and the amount must be an average for that client id.
+
+
 import pandas as pd
 
 class DataProcessor:
